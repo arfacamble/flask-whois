89697afb1ea72b2f406138afc4a3f93c9b2fd117
Arthur Campbell <arfa.camble@gmail.com>
2021-09-07 11:54:11 +0100

    venv in gitignore and basic routes

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import request
+from whois import whois
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello Poo!"
+  return '<form action="whois" method="post"><label for="domain_name">Domain:</label><input type="text" name="domain_name"></form>'
+
+@app.route("/whois", methods=['POST'])
+def whois():
+  return whois11.whois(request.form['domain_name'])
